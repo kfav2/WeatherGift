e918c32c610ba17fc094663ec3b14bf230d923ef
KFAV <kofava14@gmail.com>
2022-01-11 18:23:26 -0800

    Ch. 6.9 Adding UIPageControl "the dots" w:Tap to Navigate

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -14,6 +14,7 @@ class LocationDetailViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     var weatherLocation: WeatherLocation!
     var locationIndex = 0
@@ -32,6 +33,10 @@ class LocationDetailViewController: UIViewController {
         placeLabel.text = weatherLocation.name
         temperatureLabel.text = "--°"
         summaryLabel.text = ""
+        
+        pageControl.numberOfPages = pageViewController.weatherLocations.count
+        pageControl.currentPage = locationIndex
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -48,6 +53,13 @@ class LocationDetailViewController: UIViewController {
         let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
         pageViewController.weatherLocations = source.weatherLocations
         pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: locationIndex)], direction: .forward, animated: false, completion: nil)
+    }
+    @IBAction func pageControlTapped(_ sender: UIPageControl) {
+        let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
+        
+        let direction: UIPageViewController.NavigationDirection = ( sender.currentPage > locationIndex ? .forward : .reverse )
+        
+        pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: sender.currentPage)], direction: direction, animated: true, completion: nil)
     }
     
 }
