cf1b472874cb7203242149f7431bd2954568b75f
KFAV <kofava14@gmail.com>
2022-01-12 14:59:15 -0800

    Ch. 6.12 OpenWeather - Adding Weather Appropriate Images that Match WeatherData

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -40,6 +40,7 @@ class LocationDetailViewController: UIViewController {
                 self.placeLabel.text = self.weatherDetail.name
                 self.temperatureLabel.text = "\(self.weatherDetail.temperature)"
                 self.summaryLabel.text = self.weatherDetail.summary
+                self.imageView.image = UIImage(named: self.weatherDetail.dailyIcon)
             }
         }
     }
---
+++ WeatherGift/WeatherDetail.swift
@@ -0,0 +1,102 @@
+//
+//  WeatherDetail.swift
+//  WeatherGift
+//
+//  Created by Korlin Favara on 1/12/22.
+//
+
+import Foundation
+
+class WeatherDetail: WeatherLocation {
+    
+    struct Result: Codable {
+        var timezone: String
+        var current: Current
+        
+    }
+    
+    struct Current: Codable {
+        var dt: TimeInterval
+        var temp: Double
+        var weather: [Weather]
+    }
+    
+    struct Weather: Codable {
+        var description: String
+        var icon: String
+    }
+    
+    var timezone = ""
+    var currentTime = 0.0
+    var temperature = 0
+    var summary = ""
+    var dailyIcon = ""
+    
+    // Getting data via URLSession
+    func getData(completed : @escaping () -> ()) {
+        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIkeys.openWeatherKey)"
+        
+        print("We are accessing the url \(urlString)")
+        
+        // Create url
+        guard let url = URL(string: urlString) else {
+            print("ERROR: Could not create url from \(urlString)")
+            completed()
+            return
+        }
+        
+        // Create Session
+        let session = URLSession.shared
+        
+        // Get data with .dataTask method
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            // note: there are some additional things that could go wrong when using URLSession, but we shouldn't be experiencing them, so we will ignore testing them for now..
+            
+            // deal with the data
+            do {
+                let result = try  JSONDecoder().decode(Result.self, from: data!)
+                self.timezone = result.timezone
+                self.currentTime = result.current.dt
+                self.temperature = Int(result.current.temp.rounded())
+                self.summary = result.current.weather[0].description
+                self.dailyIcon = self.fileNameForIcon(icon: result.current.weather[0].icon)
+                
+            } catch {
+                print("JSON ERROR: \(error.localizedDescription)")
+            }
+            completed()
+        }
+        task.resume()
+    }
+    
+    private func fileNameForIcon(icon: String) -> String {
+        var newFileName = ""
+        switch icon {
+        case "01d":
+            newFileName = "clear-day"
+        case "01n":
+            newFileName = "clear-night"
+        case "02d":
+            newFileName = "partly-cloudy-day"
+        case "02n":
+            newFileName = "partly-cloudy-night"
+        case "03d", "03n", "04d", "04n":
+            newFileName = "cloudy"
+        case "09d", "09n", "10d", "10n":
+            newFileName = "rain"
+        case "11d", "11n":
+            newFileName = "thunder-storm"
+        case "13d", "13n":
+            newFileName = "snow"
+        case "50d", "50n":
+            newFileName = "fog"
+        default:
+            newFileName = ""
+        }
+        print(newFileName)
+        return newFileName
+    }
+}
